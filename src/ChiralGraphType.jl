98c0478f11e8a7e88e00efedb38d25b208617e47
Casey Kneale <ckneale@udel.com>
2020-11-16 18:19:00 -0500

    first crack at hypercomplex numbers

--- src/ChiralGraphType.jl
+++ src/ChiralGraphType.jl
@@ -1,9 +1,38 @@
-"""
-AbstractChiralGraph
-An abstract type representing a simple graph structure.
-`AbstractChiralGraph`s must have the following elements:
-- `vertices::UnitRange{Integer}`
-- `fadjlist::Vector{Vector{Integer}}`
-- `ne::Integer`
-"""
-#abstract type AbstractChiralGraph{T<:Union{Int64,Int8,Int8,Int8,Int8,Int8 }} <: AbstractGraph{T} end
+struct HyperComplex5{T}
+    a::T
+    b::T
+    c::T
+    d::T
+    e::T
+end
+
+function ⊕(x::HyperComplex5,y::HyperComplex5) 
+    promoted = promote_type(eltype(x.a), eltype(y.a))
+    HyperComplex5{promoted}(    x.a + y.a, 
+                                x.b + y.b,
+                                x.c + y.c, 
+                                x.d + y.d,
+                                x.e + y.e,
+    )
+end
+
+function ⊗(x::HyperComplex5,y::HyperComplex5) 
+    promoted = promote_type(eltype(x.a), eltype(y.a))
+    HyperComplex5{promoted}(    x.a*y.a - x.b*y.b - x.c*y.c - x.d*y.d - x.e*y.e,
+                                x.a*y.b + y.a*x.b,
+                                x.a*y.c + y.a*x.c,
+                                x.a*y.d + y.a*x.d,
+                                x.a*y.e + y.a*x.e
+    )
+end
+
+a = HyperComplex5{Int8}(1,1,1,1,1)
+b = HyperComplex5{Int8}(1,2,1,2,1)
+
+a ⊕ b
+a ⊗ b
+
+mutable struct ChiralGraph{T<:HyperComplex{Int8}} <: AbstractSimpleGraph{T}
+    ne::Int
+    fadjlist::Vector{Vector{T}} # [src]: (dst, dst, dst)
+end
